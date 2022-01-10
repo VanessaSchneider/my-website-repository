a4e0a18cee3fac2fe58101e29b66dab116427f03
Vanessa Schneider <vanessajwade@gmail.com>
2022-01-09 22:23:22 -0500

    add to script.js, connect to index.html

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
+document.querySelector("body").appendChild(h2);
+
+const img = document.querySelector('img')
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+img.addEventListener('click', clickAlert);
